349b0f9ec7f91796f1abbcd4ab73aa0906b5f3c2
bpescatori <pescatori.bg@gmail.com>
2017-05-05 00:54:17 -0300

    actualizada la branch

--- src/Location.java
+++ src/Location.java
@@ -1,20 +1,23 @@
-import java.util.Date;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.*;
-import org.*;
-import com.mysql.jdbc.Driver;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 /*
 "location":
     "city":"Cordoba";
     "country":"Argentina",
     "region":" CBA"
+
+C:\ProgramData\MySQL\MySQL Server 5.7\Data\java_bootcamp
+
  */
-public class Location {
+public class Location{
     private String city;
     private String province;
     private String country;
+    Connection conn = null;
 
     //Getters de la clase Location
     public void getCity (){
@@ -38,11 +41,57 @@ public class Location {
         this.country = newCountry;
     }
 
+    public void dbConnect() {
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/java_bootcamp" +
+                            "user=root&password=admin");
+        }catch (SQLException e){
+            System.out.println("Cannot connect - " + e);
+        }
+    }
+
+    public void dbQuery(){
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT city, province, country FROM java_bootcamp.location");
+            while (rs.next()){
+                System.out.println("DB Ciudad: "+ rs.getString("city"));
+                System.out.println("DB Provincia: "+ rs.getString("province"));
+                System.out.println("DB Pais: "+ rs.getString("country"));
+            }
+
+        }
+        catch (SQLException ex){
+            System.out.println("Cannot read - " + ex);
+        }
+        finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { }
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { }
+                stmt = null;
+            }
+        }
+    }
 
     public static void main(String[] args){
 
+
+
         //Inicializa una variable de la clase Location
         Location newLocation = new Location();
+        newLocation.dbConnect();
+        newLocation.dbQuery();
         newLocation.setCity( "Cordoba (Capital)" );
         newLocation.setProvince( "Cordoba" );
         newLocation.setCountry( "Argentina");
@@ -50,6 +99,8 @@ public class Location {
         newLocation.getProvince();
         newLocation.getCountry();
 
+
+        /*
         //Inicializa una variable de la clase Atmosphere
         Atmosphere newAtmosphere = new Atmosphere();
         newAtmosphere.setHumidity( 90 );
@@ -78,7 +129,7 @@ public class Location {
             newWeather[i].setWDay( dia[ i%7 ] );
             newWeather[i].setTempMin( 21-i%3 );
             newWeather[i].setTempMax( 27+i%3 );
-        };
+        }*/
 
     }
 }
